a04d7832661aa07e236339cff6f1cf6665658edf
Torey Hickman <torey@toreyhickman.com>
2015-05-11 12:45:13 -0500

    Working synchronous code

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,8 @@
 get '/' do
   @sign_text = params[:sign_text]
-  # Look in app/views/index.erb
   erb :index
 end
 
 post '/cheers' do
-  "We are not handling POST requests to '/cheers' the way we want to.<br>We'll need to fix this.<br><br>Here's a look at the params hash: <code>#{params.inspect}</code>"
+  redirect "/?sign_text=#{Mascot.sign_for(params[:cheer_name])}"
 end
